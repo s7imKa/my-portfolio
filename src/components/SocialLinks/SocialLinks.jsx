3e1128ca17691dfc427203fc164bd61df0e36410
s7imKa <maksikmaksik2005@gmail.com>
2025-10-24 19:13:15 +0300

    fix: correct image import paths in SocialLinks component

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -7,22 +7,22 @@ const SocialLinks = ({ column = false }) => {
             <ul className={classSocialIconUl}>
                 <li className='social-sidebar__li'>
                     <a href='#' className='social-sidebar__a a1'>
-                        <img src='/src/assets/icons/social-link/github.svg' alt='git hub' />
+                        <img src='../../assets/icons/social-link/github.svg' alt='git hub' />
                     </a>
                 </li>
                 <li className='social-sidebar__li'>
                     <a href='#' className='social-sidebar__a a2'>
-                        <img src='/src/assets/icons/social-link/Telegram.svg' alt='telegram' />
+                        <img src='../../assets/icons/social-link/Telegram.svg' alt='telegram' />
                     </a>
                 </li>
                 <li className='social-sidebar__li'>
                     <a href='#' className='social-sidebar__a a3'>
-                        <img src='/src/assets/icons/social-link/Email.svg' alt='telegram' />
+                        <img src='../../assets/icons/social-link/Email.svg' alt='telegram' />
                     </a>
                 </li>
                 <li className='social-sidebar__li'>
                     <a href='#' className='social-sidebar__a a4'>
-                        <img src='/src/assets/icons/social-link/Figma.svg' alt='telegram' />
+                        <img src='../../assets/icons/social-link/Figma.svg' alt='telegram' />
                     </a>
                 </li>
             </ul>
