711b1dc63d430481d6ec37e3115d5540d79f8c0a
s7imKa <maksikmaksik2005@gmail.com>
2025-10-25 15:31:54 +0300

    Refactor code structure for improved readability and maintainability

--- src/layouts/HeroSection/HeroSection.jsx
+++ src/layouts/HeroSection/HeroSection.jsx
@@ -5,23 +5,28 @@ import Button from '../../components/Button/Button'
 const HeroSection = () => {
     return (
         <section className='hero-section'>
-            <div className='here-wrap container'>
-                <div className='here-content'>
-                    <h1 className='here-content__title'>
+            <div className='hero-wrap container'>
+                <div className='hero-content'>
+                    <h1 className='hero-content__title'>
                         Maksym is a <span>front-end developer</span>
                     </h1>
-                    <p className='here-content__description'>
+                    <p className='hero-content__description'>
                         He crafts responsive websites where technologies meet creativity
                     </p>
-                    <Button gray={false}>Contact me!! {'<~>'}</Button>
-                    <Button gray={true}>Contact me!! {'<~>'}</Button>
+                    <Button gray={false}>Contact me!!</Button>
                 </div>
-                <div className='here-avatar'>
-                    <div className='here-avatar__photo'>
+                <div className='hero-avatar'>
+                    <div className='hero-avatar__photo'>
                         <img src='/icons/site-efect-icon/Dots.svg' alt='' />
                         <img src='/icons/site-efect-icon/Logo.svg' alt='' />
                     </div>
-                    <div className='here-avatar__status-bar'></div>
+                    <div className='hero-avatar__status-bar'>
+                        <span className='hero-avatar__status-bar__box'></span>
+
+                        <p className='hero-avatar__status-bar__text'>
+                            Currently working on <span>Portfolio</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
